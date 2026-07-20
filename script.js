de7a3e80e68a2450a4aabf232c88463bc9b0e180
Niyati <109666612+shah-1245@users.noreply.github.com>
2026-07-20 17:10:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+
+    catch{
+        display.value = "Error";
+    }
+
+}
+
+// Keyboard Support
+
+document.addEventListener("keydown",function(event){
+
+    let key = event.key;
+
+    if("0123456789+-*/.".includes(key)){
+        appendValue(key);
+    }
+
+    else if(key==="Enter"){
+        calculate();
+    }
+
+    else if(key==="Backspace"){
+        deleteLast();
+    }
+
+    else if(key==="Escape"){
+        clearDisplay();
+    }
+
+});
